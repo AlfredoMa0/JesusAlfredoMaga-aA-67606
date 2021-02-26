3292d7aa2d0534f1383888d03af46fd9efc1f2b2
Campe <Campe@LAPTOP-F5OP52OL>
2021-02-25 20:08:30 -0600

    TAREA 1.0

---
+++ src/jesusalfredomagañaa/pkg67606/JesusAlfredoMagañaA67606.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jesusalfredomagañaa.pkg67606;
+
+/**
+ *
+ * @author Campe
+ */
+public class JesusAlfredoMagañaA67606 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "JESUS ALFREDO MAGAÑA AGUILAR";
+        System.out.println("MI NOMBRE ES:" + nombre);
+        
+    }
+    
+}
